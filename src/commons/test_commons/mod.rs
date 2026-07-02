03142a8fdc59e14d7b898805d928e66f245d62cb
Luiz Silveira <zertyz@gmail.com>
2026-07-01 23:36:47 -0300

    Addressed CI issues

--- src/commons/test_commons/mod.rs
+++ src/commons/test_commons/mod.rs
@@ -1,14 +1,17 @@
 //! Contains code used across different tests
 
+use anyhow::{anyhow, Result};
+
 pub mod db_test_commons;
 
 // enable logging for unit tests
 #[cfg(test)]
 #[ctor::ctor(unsafe)]
-static TEST_LOGGER_GUARD: ftlog::LoggerGuard = {
+static TEST_LOGGER_GUARD: Result<ftlog::LoggerGuard> = {
     ftlog::builder()
         .max_log_level(log::LevelFilter::Info)
         .bounded(16, true)
         .try_init()
-        .expect("could not initialize logging for tests")
+        .map_err(|err| anyhow!("Could not initialize logging for tests: {err}"))
+        .inspect_err(|err| eprintln!("### {err}"))
 };
